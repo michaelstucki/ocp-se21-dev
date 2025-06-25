5d9811630e398e7db138d81cccf44d9ab3483477
Michael Stucki <stcuthbertus@gmail.com>
2025-06-25 09:59:55 -0600

    interning - shared heap memory pool for strings

---
+++ interning/Main.java
@@ -0,0 +1,9 @@
+package interning;
+class Main {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "hello";
+        System.out.println(a == b);
+    }
+}
+
